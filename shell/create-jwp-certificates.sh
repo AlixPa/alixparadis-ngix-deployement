692a24156dfdb3700ffc4f86258e1c7fa8a0f9db
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-11-16 01:50:51 +0900

    non interactive script

--- shell/create-jwp-certificates.sh
+++ shell/create-jwp-certificates.sh
@@ -3,5 +3,8 @@ docker run --rm \
   -v $(pwd)/certbot/www:/var/www/certbot \
   -v $(pwd)/certbot/conf:/etc/letsencrypt \
   certbot/certbot certonly \
+  --non-interactive \
+  --agree-tos \
+  --register-unsafely-without-email \
   --webroot -w /var/www/certbot \
   -d jwp.alixparadis.com
