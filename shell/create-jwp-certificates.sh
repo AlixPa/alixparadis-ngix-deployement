40df00be41162e54521bbb0eb10367c002e4af33
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-11-16 01:58:47 +0900

    dummy certificate

--- shell/create-jwp-certificates.sh
+++ shell/create-jwp-certificates.sh
@@ -1,4 +1,10 @@
 #/!/bin/sh
+mkdir -p certbot/conf/live/jwp.alixparadis.com
+openssl req -x509 -nodes -days 1 \
+  -newkey rsa:2048 \
+  -keyout certbot/conf/live/jwp.alixparadis.com/privkey.pem \
+  -out certbot/conf/live/jwp.alixparadis.com/fullchain.pem \
+  -subj "/CN=jwp.alixparadis.com"
 docker compose up -d nginx
 docker run --rm \
   -v $(pwd)/certbot/www:/var/www/certbot \
@@ -9,3 +15,4 @@ docker run --rm \
   --register-unsafely-without-email \
   --webroot -w /var/www/certbot \
   -d jwp.alixparadis.com
+docker exec nginx nginx -s reload
