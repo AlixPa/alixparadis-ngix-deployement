23fe5944e63cb770bf7f6f211345ad210d2506b8
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-11-16 03:02:38 +0900

    correct folder

--- shell/create-jwp-certificates.sh
+++ shell/create-jwp-certificates.sh
@@ -11,7 +11,7 @@ TMP_FOLDER=certbot/tmp
 mkdir $TMP_FOLDER
 docker run --rm \
   -v $(pwd)/certbot/www:/var/www/certbot \
-  -v $TMP_FOLDER:/etc/letsencrypt/conf/live/jwp.alixparadis.com \
+  -v $TMP_FOLDER:/etc/letsencrypt \
   certbot/certbot certonly \
   --non-interactive \
   --agree-tos \
@@ -20,6 +20,6 @@ docker run --rm \
   --webroot -w /var/www/certbot \
   -d jwp.alixparadis.com
 
-mv $TMP_FOLDER certbot/conf/live/jwp.alixparadis.com
+mv $TMP_FOLDER/* certbot/conf/
 docker exec nginx nginx -s reload
 docker compose up -d certbot
