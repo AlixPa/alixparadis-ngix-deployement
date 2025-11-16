a7bc778375d30dbfdb5bae2ccefa4a54ad2447dc
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-11-16 14:39:38 +0900

    test file

---
+++ shell/test.sh
@@ -0,0 +1,10 @@
+#/!/bin/sh
+docker compose stop
+rm -rf certbot
+mkdir -p certbot/conf/live/jwp.alixparadis.com
+openssl req -x509 -nodes -days 1 \
+  -newkey rsa:2048 \
+  -keyout certbot/conf/live/jwp.alixparadis.com/privkey.pem \
+  -out certbot/conf/live/jwp.alixparadis.com/fullchain.pem \
+  -subj "/CN=jwp.alixparadis.com"
+docker compose up -d nginx
